6f0328902507ec726f5e522f2c8aaae2ed0fc816
Warsama ILMI AMIR <warsama.ilmiamir@gmail.com>
2017-05-23 10:31:34 +0200

    4eme commit stable (à finir)

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -2,26 +2,36 @@ package dev.sgp.web;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dev.sgp.entite.Collaborateur;
+import dev.sgp.service.CollaborateurService;
+import dev.sgp.util.Constantes;
+
 public class ListerCollaborateursController extends HttpServlet {
+
+	// récupération du service
+
+	private CollaborateurService collabService = Constantes.COLLAB_SERVICE;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 
 	ServletException, IOException {
 
+		// utilisation du service
+
+		List<Collaborateur> collaborateurs = collabService.listerCollaborateurs();
+		
 		// A ajouter
-
-		req.setAttribute("listeNoms", Arrays.asList("Gertrude", "Alison", "Youri"));
-
+		req.setAttribute("collab", collaborateurs);
 		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
-
 				.forward(req, resp);
-	}
 
+	}
 }
